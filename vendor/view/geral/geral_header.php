54aeea991afb9dc2c8861de3bac5120b1db345a3
Kenio <kenio_souza@hotmail.com>
2025-07-03 16:58:28 -0300

    feat: Adiciona novas imagens e atualiza o cabeçalho geral
    - Adiciona LOGOMARCA2.png e logo.png na pasta image/.
    - Atualiza vendor/view/geral/geral_header.php.

--- vendor/view/geral/geral_header.php
+++ vendor/view/geral/geral_header.php
@@ -8,8 +8,7 @@
 
     <h4 class="mb-0">
 
-        <img src="image/softwiki.png" width="27" alt="MyCMS - Souza Consultoria Tecnológica" loading="lazy"> |
-        <img src="image/sct.png" width="30" alt="MyCMS - Souza Consultoria Tecnológica" loading="lazy">
+        <img src="image/logo.png" width="27" alt="Mydocs - Orius Tecnologia" loading="lazy">
         <strong>My</strong>Docs
 
     </h4>
