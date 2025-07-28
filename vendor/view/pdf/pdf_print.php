be1d1608860a082cdedf026a0d0b42218bd2c309
Kenio de Souza <kenio@outlook.com>
2025-07-28 16:09:10 -0300

    Atualizando drafts_companies_print.php

--- vendor/view/pdf/pdf_print.php
+++ vendor/view/pdf/pdf_print.php
@@ -6,7 +6,7 @@
   <meta name="author" content="<?php echo $_SESSION['USER_NAME'] ?>">
   <meta name="keywords" content="<?php echo $resultDraftCompanies->name ?>">
   <style>
-    /*html, body, div, span, applet, object, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, pre, a, abbr, acronym, address, big, cite, code, del, dfn, em, img, ins, kbd, q, s, samp, small, strike, strong, sub, sup, tt, var, b, u, i, center, dl, dt, dd, ol, ul, li, fieldset, form, label, legend, table, caption, tbody, tfoot, thead, tr, th, td, article, aside, canvas, details, embed, figure, figcaption, footer, header, hgroup, menu, nav, output, ruby, section, summary, time, mark, audio, video {
+    html, body, div, span, applet, object, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, pre, a, abbr, acronym, address, big, cite, code, del, dfn, em, img, ins, kbd, q, s, samp, small, strike, strong, sub, sup, tt, var, b, u, i, center, dl, dt, dd, ol, ul, li, fieldset, form, label, legend, table, caption, tbody, tfoot, thead, tr, th, td, article, aside, canvas, details, embed, figure, figcaption, footer, header, hgroup, menu, nav, output, ruby, section, summary, time, mark, audio, video {
      margin: 0;
      padding: 0;
      border: 0;
@@ -14,13 +14,13 @@
      font: inherit;
      vertical-align: baseline;
 }
- */
+
     /* HTML5 display-role reset for older browsers */
-    /* article, aside, details, figcaption, figure, footer, header, hgroup, menu, nav, section {
+    article, aside, details, figcaption, figure, footer, header, hgroup, menu, nav, section {
      display: block;
 }
  body {
-     line-height: 1;
+     line-height: 1.5;
 }
  ol, ul {
      list-style: none;
@@ -32,7 +32,6 @@
      content: '';
      content: none;
 }
-*/
     @page {
       size: <?php echo $resultConfiguration->preferences->page->width?>cm <?php echo $resultConfiguration->preferences->page->height?>cm !important;
       margin-left: <?php echo $resultConfiguration->preferences->page->body->margin_left?>cm !important;
